479df4157ecb2d73b2f313140e255d4544bc0819
ale <alejandrodanieltelmo@gmail.com>
2021-10-16 20:17:24 -0300

    arreglando tablas

--- multiplicador/index.js
+++ multiplicador/index.js
@@ -3,19 +3,19 @@ const fs=require("fs")
 const multiplicar=(base)=>{
     //const base=3;
     console.log(`==============Tabla del ${base}==============`);
-    for(let juab=1;juab<=10;juab++){
-        console.log(`${base} * ${juab} = ${base*juab}`);
+    for(let numero=1;numero<=10;numero++){
+        console.log(`${base} * ${numero} = ${base*numero}`);
     }
     
     console.log("Hasta aca sin guardar");
     console.log("Desde  aca  guardadando====================================");
     console.log(`==============Tabla del ${base}==============`);
     let resultado;
-    for(let juab=1;juab<=10;juab++){
-       resultado+=`${base} * ${juab} = ${base*juab}\n`;
+    for(let numero=1;numero<=10;numero++){
+       resultado+=`${base} * ${numero} = ${base*numero}\n`;
     }
     console.log(resultado);
-    fs.writeFileSync(`Tablas/tablas-del-${base}`,resultado);
+    fs.writeFileSync(`Tablas/tablas-del-${base}.txt`,resultado);
 }
 
 
