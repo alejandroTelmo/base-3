a6e6f5dda4aef94f3c090d0b7ad00206e9bdbcb8
ale <alejandrodanieltelmo@gmail.com>
2021-10-16 21:10:10 -0300

    yarg con mas opciones

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+const argv=require("yargs")
+.option('b',{
+    alias:'base',
+    type:'number',
+    demandOption:true,
+    describe:'Es la base de la tabla de multiplicar'
+}
+).option('l',{
+    alias:'limite',
+    type:'number',
+    demandOption:false,//aca estoy diciendo que no va a ser requerido...es opcional
+    default:10,
+    describe:'Establece un limite a la ejecución del código.'
+})
+.version()//el valor que nos devuelve x consola lo sava del package.json
+.help()
+.check((argv,options)=>{
+    if(isNaN(argv.b)){
+        throw Error('El argumento tiene que ser un número.')
+    }
+    return true;
+}).argv;
+
+//const { argv } = require("yargs");
 const {multiplicar}=require("./multiplicador/index");
 const base=5;
-multiplicar(base);
+multiplicar(argv.b,argv.l);
+//console.log(argv);
--- multiplicador/index.js
+++ multiplicador/index.js
@@ -1,17 +1,17 @@
 
 const fs=require("fs")
-const multiplicar=(base)=>{
+const multiplicar=(base,limite)=>{
     //const base=3;
     console.log(`==============Tabla del ${base}==============`);
-    for(let numero=1;numero<=10;numero++){
+    for(let numero=1;numero<=limite;numero++){
         console.log(`${base} * ${numero} = ${base*numero}`);
     }
     
     console.log("Hasta aca sin guardar");
     console.log("Desde  aca  guardadando====================================");
     console.log(`==============Tabla del ${base}==============`);
-    let resultado;
-    for(let numero=1;numero<=10;numero++){
+    let resultado='';
+    for(let numero=1;numero<=limite;numero++){
        resultado+=`${base} * ${numero} = ${base*numero}\n`;
     }
     console.log(resultado);
